404f5d620e3a812096d85a47ead2b28374cfcca7
Adam Erdem Er <adamerdemer@gmail.com>
2017-05-11 14:12:23 +0100

    Added thermostat source and test files with content

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,16 @@
+describe('Thermostat', function(){
+  var thermostat;
+
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  });
+
+  it('thermostat starts at 20 degrees', function(){
+    expect(thermostat.temperature).toEqual(20);
+  });
+
+  it('can increase the temperature with an up function',function(){
+    thermostat.UpTemp();
+    expect(thermostat.temperature).toEqual(21);
+  });
+});
